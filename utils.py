68abb9a836f626bc9e758102b3f1f5f1c5ef8b80
divyaK15 <divya.p.k15@gmail.com>
2023-09-08 13:07:42 -0400

    errorr

--- utils.py
+++ utils.py
@@ -2,9 +2,11 @@ class utils:
     def resversed(self, number):
         if type(number) is int:
             return int(str(number)[::-1])
+        print("error")
         return None
 
     def formatter(self, number):
         if type(number) is int: 
             return bin(number), oct(number)
+        print("error")
         return None
