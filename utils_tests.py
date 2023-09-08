1f36caacddf3f31889cf6d3659b3d0562015ee1f
divyaK15 <divya.p.k15@gmail.com>
2023-09-08 13:29:17 -0400

    added tests

---
+++ utils_tests.py
@@ -0,0 +1,27 @@
+from utils import utils
+
+def reversed_int():
+    myClass = utils()
+    myClass.reversed(12345)
+
+def reversed_float():
+    myClass = utils()
+    myClass.reversed(35.3)
+
+def reversed_string():
+    myClass = utils()
+    myClass.reversed("1234")
+
+def format_int():
+    myClass = utils()
+    myClass.formatter(123)
+
+def format_float():
+    myClass = utils()
+    myClass.formatter(12.3)
+
+def format_string():
+    myClass = utils()
+    myClass.formatter("123")
+
+
