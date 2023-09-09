bedf51cdff8abe6efd4ed1f3175b985d4ce8286c
divyaK15 <divya.p.k15@gmail.com>
2023-09-09 11:30:03 -0400

    added print statements

--- utils.py
+++ utils.py
@@ -1,12 +1,24 @@
 class utils:
     def reversed(self, number):
         if type(number) is int:
-            return int(str(number)[::-1])
-        print("error")
+            #result = int(str(number)[::-1])
+            result = 0
+            while (number > 0):
+                last = number % 10
+                result = (result * 10) + last 
+                number = number // 10
+            print("reversed int:",result)
+            return result
+        print("reversed: error not an int")
         return None
 
     def formatter(self, number):
         if type(number) is int: 
-            return bin(number), oct(number)
-        print("error")
+            binresult = bin(number)
+            octresult = oct(number)
+            print("binary:", binresult)
+            print("octal:", octresult)
+            return binresult, octresult
+        print("format: error not an int")
         return None
+
